8627ec467ea63844d0e47519106636ac94ddef9d
Christine Beck <cpbcpb@users.noreply.github.com>
2019-01-30 17:00:56 -0500

    Format a contents page for a book I made up

---
+++ tableOfContents.rb
@@ -0,0 +1,13 @@
+lineWidth=78
+title='The Friendly Ferengi'
+puts("*"*lineWidth)
+puts(title.center(lineWidth))
+puts(" ")
+
+puts('Table of Contents'.center(lineWidth))
+puts(" ")
+puts('Chapter 1: The Buyer Beware'.ljust(lineWidth/2) + 'page 1'.rjust(lineWidth/2))
+puts('Chapter 2: The Rules of Aquisition'.ljust(lineWidth/2) + 'page 72'.rjust(lineWidth/2))
+puts('Chapter 3: What Is Gold Pressed Latinum'.ljust(lineWidth/2) + 'page 118'.rjust(lineWidth/2))
+puts("*"*lineWidth)
+
