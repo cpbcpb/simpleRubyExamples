8e9764f6f4fcb15a324f8d3299b3c517c3ede142
Christine Beck <cpbcpb@users.noreply.github.com>
2019-01-31 12:02:22 -0500

    more simple programs

---
+++ wordSorter.rb
@@ -0,0 +1,24 @@
+#version 2, not using array.sort method
+# puts 'Type in some words'
+# word=gets.chomp
+# words=[]
+# while word != ''
+# 	words.push word
+# 	word = gets.chomp
+# end
+
+# words.each do |aWord|
+	
+# end
+# puts words.sort
+
+
+#version 1, using array.sort method
+puts 'Type in some words'
+word=gets.chomp
+words=[]
+while word != ''
+	words.push word
+	word = gets.chomp
+end
+puts words.sort
